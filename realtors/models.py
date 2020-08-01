25f373c30f0b690cf528e3b5275a7750212c9f53
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-08-01 17:26:07 +0430

    models created

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="photos/%y/%m/%d/")
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.name
